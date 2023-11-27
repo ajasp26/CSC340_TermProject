34361d068a8b1339e57e4e8df94a42633b5e9f1d
Derek Fox <derekefox@gmail.com>
2023-11-27 14:42:09 -0500

    added docs

--- scam-guard-main/src/main/java/com/csc340/scamguard/AppController.java
+++ scam-guard-main/src/main/java/com/csc340/scamguard/AppController.java
@@ -4,13 +4,18 @@ import com.csc340.scamguard.admin.AdminService;
 import com.csc340.scamguard.business.BusinessService;
 import com.csc340.scamguard.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Collection;
+
 /**
- *
+ * Controller for the main application.
  * @author Derek Fox
  */
 @Controller
@@ -25,20 +30,29 @@ public class AppController {
     @Autowired
     AdminService adminService;
 
+    /**
+     * Returns the dashboard page for the current user.
+     */
     @GetMapping(value = {"", "/", "/dashboard", "/home"})
     public String dashboard(Model model) {
-        String name = SecurityContextHolder.getContext().getAuthentication().getName();
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+
+        String name = auth.getName();
         model.addAttribute("currentName", name);
 
-        if (businessService.getBusinessByTitle(name) != null) {
+        /*
+         * Get the current user's authority and return the appropriate dashboard.
+         */
+        Collection<? extends GrantedAuthority> authList = auth.getAuthorities();
+        if (authList.contains(new SimpleGrantedAuthority("BUSINESS"))) {
             return "business/menu";
-        } else if (userService.getUserByUserName(name) != null) {
+        } else if (authList.contains(new SimpleGrantedAuthority("USER"))) {
             return "user/menu";
-        } else if (adminService.getAdminByName(name) != null){
+        } else if (authList.contains(new SimpleGrantedAuthority("ADMIN"))){
             return "admin/menu";
         }
 
-        throw new IllegalStateException("user has no role");
+        throw new IllegalStateException("user has no authority");
     }
 
     @GetMapping("/login")
--- scam-guard-main/src/main/java/com/csc340/scamguard/Client.java
+++ scam-guard-main/src/main/java/com/csc340/scamguard/Client.java
@@ -1,4 +1,7 @@
 package com.csc340.scamguard;
 
+/**
+ * Used this for a hack in the CustomUserDetailsService class.
+ */
 public class Client {
 }
